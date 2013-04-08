dca1b4e9426b29bf8beff4a4c73b732233b7bb3f
Jon Neely <jneely@solidyn.com>
2013-04-08 12:14:55 -0600

    basic file selection/content display complete

--- assets/js/wordCount.js
+++ assets/js/wordCount.js
@@ -1,4 +1,31 @@
+$(document).ready(function () {
+    $('#inputFile').change(function (evt) {
+        console.log("Selected input file: " + evt.target.files[0].name);
+    })
+
+    $('#processBtn').click(processFile);
+})
+
+
+
 // Simple word counter
-function processFile(file) {
-    console.log("Called processFile")
+function processFile() {
+    var fileToProcess = $('#inputFile')[0].files[0];
+
+    if (fileToProcess === undefined) {
+        alert("Select a text file for processing");
+        return;
+    }
+
+    console.log("Called processFile for " + fileToProcess.name + ", bytes: "+fileToProcess.size);
+
+    var reader = new FileReader();
+    reader.onload = function(theFile) {
+        var contents = theFile.target.result;
+
+        $('#fileOutput').html("<pre>"+contents+"</pre>");
+        console.log("Added text done")
+    }
+
+    reader.readAsText(fileToProcess)
 }
